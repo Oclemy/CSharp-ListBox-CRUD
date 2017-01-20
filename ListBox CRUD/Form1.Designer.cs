ff4fe9e0ce4083ac58792eda87546b874680f9ee
Oclemy <oclemmi@gmail.com>
2017-01-19 21:39:37 -0800

    Initial Commit

---
+++ ListBox CRUD/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ListBox_CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.nameTxt = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.addBtn = new MetroFramework.Controls.MetroButton();
+            this.updateBtn = new MetroFramework.Controls.MetroButton();
+            this.deleteBtn = new MetroFramework.Controls.MetroButton();
+            this.clearBtn = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(24, 95);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(292, 316);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // nameTxt
+            // 
+            // 
+            // 
+            // 
+            this.nameTxt.CustomButton.Image = null;
+            this.nameTxt.CustomButton.Location = new System.Drawing.Point(174, 1);
+            this.nameTxt.CustomButton.Name = "";
+            this.nameTxt.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.nameTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.nameTxt.CustomButton.TabIndex = 1;
+            this.nameTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.nameTxt.CustomButton.UseSelectable = true;
+            this.nameTxt.CustomButton.Visible = false;
+            this.nameTxt.Lines = new string[0];
+            this.nameTxt.Location = new System.Drawing.Point(401, 126);
+            this.nameTxt.MaxLength = 32767;
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.PasswordChar = '\0';
+            this.nameTxt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.nameTxt.SelectedText = "";
+            this.nameTxt.SelectionLength = 0;
+            this.nameTxt.SelectionStart = 0;
+            this.nameTxt.ShortcutsEnabled = true;
+            this.nameTxt.Size = new System.Drawing.Size(196, 23);
+            this.nameTxt.TabIndex = 1;
+            this.nameTxt.UseSelectable = true;
+            this.nameTxt.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.nameTxt.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(323, 126);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(52, 19);
+            this.metroLabel1.TabIndex = 2;
+            this.metroLabel1.Text = "Name :";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(334, 205);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(85, 40);
+            this.addBtn.TabIndex = 3;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseSelectable = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(466, 205);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(85, 40);
+            this.updateBtn.TabIndex = 4;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseSelectable = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(334, 277);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(85, 40);
+            this.deleteBtn.TabIndex = 5;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseSelectable = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(466, 277);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(85, 40);
+            this.clearBtn.TabIndex = 6;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseSelectable = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 458);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.nameTxt);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "ListBox CRUD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroTextBox nameTxt;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroButton addBtn;
+        private MetroFramework.Controls.MetroButton updateBtn;
+        private MetroFramework.Controls.MetroButton deleteBtn;
+        private MetroFramework.Controls.MetroButton clearBtn;
+    }
+}
+
---
+++ ListBox CRUD/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using MetroFramework.Forms;
+
+namespace ListBox_CRUD
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+        * ITEM SELECTED
+        */
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index != -1)
+            {
+                nameTxt.Text = listBox1.SelectedItem.ToString();
+
+            }
+        }
+
+        /*
+        *ADD
+        */
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            string name = nameTxt.Text;
+            listBox1.Items.Add(name);
+            clearTxt();
+        }
+
+        /*
+        * UPDATE
+        */
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index != -1)
+            {
+                listBox1.Items.RemoveAt(index);
+                listBox1.Items.Insert(index, nameTxt.Text);
+            }
+           
+        }
+
+        /*
+        * DELETE
+        */
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            if (index != -1)
+            {
+                listBox1.Items.RemoveAt(index);
+                clearTxt();
+            }
+        }
+
+        /*
+        * CLEAR
+        */
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            clearTxt();
+        }
+
+        /*
+         * CLEAR TEXTBOX
+         */
+        private void clearTxt()
+        {
+            nameTxt.Text = "";
+        }
+    }
+}
